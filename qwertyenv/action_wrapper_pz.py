de4d078bc3770891e1339dcd3433be61ddd4add9
Oren <zbenmo@gmail.com>
2023-03-13 14:24:13 +0100

    Collect coins for PZ has now a companion "wrapper" that makes a PZ env into a Gymnasium environment.

--- qwertyenv/action_wrapper_pz.py
+++ qwertyenv/action_wrapper_pz.py
@@ -6,6 +6,9 @@ class ActionWrapper(BaseWrapper):
   def __init__(self, env: gym.Env):
     super().__init__(env)
 
+  # def reset(self, seed=None, return_info=True, options=None):
+  #   self.env.reset(seed=seed, return_info=return_info, options=options)
+
   def step(self, action):
     action = self.action(action)
     self.env.step(action)
@@ -15,3 +18,11 @@ class ActionWrapper(BaseWrapper):
 
   def render(self, *args, **kwargs):
     self.env.render(*args, **kwargs)
+
+  @property
+  def agent_selection(self):
+    return self.env.agent_selection
+  
+  @agent_selection.setter
+  def agent_selection(self, agent):
+    self.env.agent_selection = agent
